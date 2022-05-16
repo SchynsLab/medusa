8ee19b18ae1ee523f4699a0146c49fd069e92046
Lukas Snoek <lukassnoek@gmail.com>
2022-05-16 16:03:34 +0100

    Clean up docs

--- docs/autoapi/conf.py
+++ docs/autoapi/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-autoapi_dirs = ['../../gmfx']
+autoapi_dirs = ['../../medusa']
 autoapi_options = ['members', 'undoc-members', 'show-inheritance', 'special-members']
 
 
