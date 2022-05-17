f0c371115919698d38460cc509be9d3f11556047
Lukas Snoek <lukassnoek@gmail.com>
2022-05-17 11:27:45 +0100

    Add doctest to make pytest pass on CI

--- medusa/utils.py
+++ medusa/utils.py
@@ -2,7 +2,23 @@ import logging
 
 
 def get_logger(verbose="INFO"):
-
+    """ Create a Python logger.
+    
+    Parameters
+    ----------
+    verbose : str
+        Logging level ("INFO", "DEBUG", "WARNING")
+    
+    Returns
+    -------
+    logger : logging.Logger
+        A Python logger
+    
+    Examples
+    --------
+    >>> logger = get_logger()
+    >>> logger.info("Hello!")
+    """
     logging.basicConfig(
         level=getattr(logging, verbose),
         format="%(asctime)s [%(levelname)-7.7s]  %(message)s",
