35bd180d3906b444c6995d7fa801510d75a61182
Lukas Snoek <lukassnoek@gmail.com>
2022-12-12 13:00:33 +0000

    Fix precommit

--- ci/download_data.sh
+++ ci/download_data.sh
@@ -1,1 +1,1 @@
-yes 2>/dev/null | medusa_download_ext_data --username lukassnoek@gmail.com --password "$passwd_flame"
+yes 2>/dev/null | medusa_download_ext_data --username lukassnoek@gmail.com --password "$passwd_flame" --directory $HOME/.medusa_ext_data
